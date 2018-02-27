2856d504a3cd2a7ed6a654cc9d0562de6fc985e9
Nimrod O. Abrigo <abrigonimrod@gmail.com>
2018-02-27 15:14:49 +0800

    Implement coinmarketcap API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Coin } from './coin';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +11,13 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 })
 export class AppComponent {
   title = 'app';
+  readonly ROOT_URL='https://api.coinmarketcap.com/v1/';
+
+  posts:Observable<any[]>;
+
+  constructor(private http:HttpClient){}
+
+  getPosts(){
+    this.posts = this.http.get<Coin[]>(this.ROOT_URL+'/ticker/ethereum');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatTableModule } from '@angular/material/table';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MatCheckboxModule,
-    MatTableModule,
+    HttpClientModule,
     NgbModule.forRoot()
   ],
   providers: [],
